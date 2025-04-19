9d71eb00903d0c0e0e51fa2234cee611f36e707d
Lewie Hodgkins <lewie.s.hodgkins@gmail.com>
2025-04-19 17:24:33 +0100

    make it offensive to everyone

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ while True:
     answer = input(question)
     if answer == "":
         break
-    elif "leon" in answer.lower():
-        print("you are a clown!")
-    else:
+    elif "lewie" in answer.lower():
         print(f"hello, {answer}")
+    elif "leon" in answer.lower():
+        print("you are a very special clown!")
+    else:
+        print(f"hello, clown")
 
 print("goodbye!")
