dba9f3de9aedc125fda4ac06ce1d0c2d9d2ee742
Lewie Hodgkins <lewie.s.hodgkins@gmail.com>
2025-04-20 11:54:33 +0100

    what planet are you from ver 1

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+print ;"hello"
+
+question = "what planet are you from? " 
+
+while True:
+    answer = input (question)
+    if answer == "": 
+       break
+    elif "earth" in answer. lower():
+         print("hello my fellow earthling")           
+    elif "mars" in answer. lower(): 
+         print ("ahhh its an ailien")
+
+print ("goodbye")            
